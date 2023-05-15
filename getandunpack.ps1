3001e6ecbca7f51a6e4d0e8b5cdf613026569620
Marc Kielnik <53785384+tazer73@users.noreply.github.com>
2023-05-15 08:30:20 -0500

    Core files
    Main files

---
+++ getandunpack.ps1
@@ -0,0 +1,19 @@
+$networkname = Read-Host -Prompt "Enter an IP address or node name"
+$workingdir = Read-Host -Prompt "Please enter the file path along with your name for the file (C:\Users\User\test.edpk)"
+$password = Read-Host -Prompt "Please enter the password"
+$settingcmdpath = "C:\Program Files\SettingCommand\settingcmd.exe"
+$ipRegex = "^(\d{1,3}\.){3}\d{1,3}$"
+$nodeRegex = "^[a-zA-Z0-9\-]+$"
+
+if ($networkname -match $ipRegex) {
+    & "$settingcmdpath" retrieve --ip "$networkname" --o "$workingdir" --password "$password"
+} elseif ($networkname -match $nodeRegex) {
+    & "$settingcmdpath" retrieve --node "$networkname" --o "$workingdir" --password "$password"
+} else {
+    Write-Host "Invalid input."
+    Pause
+    exit
+}
+& "$settingcmdpath" unpack --file "$workingdir" --unpackdir (Split-Path $workingdir -Parent) --password "$password"
+
+Pause
---
+++ unpacker.ps1
@@ -0,0 +1,8 @@
+$filepath = Read-Host -Prompt "Please enter the file path along with your name for the file (C:\Users\User\test.edpk)"
+$outputpath = Read-Host -Prompt "Enter the output path:"
+$password = Read-Host -Prompt "Please enter the password"
+$settingcmdpath = "C:\Program Files\SettingCommand\settingcmd.exe"
+
+& "$settingcmdpath" unpack --file "$filepath" --unpackdir "$outputpath" --password "$password"
+
+Pause
